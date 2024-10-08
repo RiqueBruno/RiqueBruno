b50e8037f588378a5f135ab544fc6655780c779f
Bruno H <riquebruno@yahoo.com.br>
2024-10-08 19:44:56 -0300

    feat: cria componente Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logoDark from '../../assets/logo/dark.png';
+import logoLight from '../../assets/logo/light.png';
+
+type LogoProps = {
+  darkMode: boolean;
+  className: string;
+};
+
+export default function Logo({ darkMode, className }: LogoProps) {
+  return (
+    <img
+      src={darkMode ? logoDark : logoLight}
+      alt="Logo portfólio Bruno Henrique"
+      className={className}
+    />
+  );
+}
