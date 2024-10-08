dfb9d7dce610f2358457798ae47466760e2557cd
Bruno H <riquebruno@yahoo.com.br>
2024-10-08 11:18:37 -0300

    refactor: Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Aqui estamos incluindo todas as extensões possíveis.
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
