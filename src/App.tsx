9892f15917a9401e24e78506e16a2e278eead0ad
Bruno H <riquebruno@yahoo.com.br>
2024-10-08 19:09:48 -0300

    feat: Add ThemeButton component for toggling between dark and light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import ThemeButton from './components/ThemeButton/ThemeButton';
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -6,13 +7,47 @@ function App() {
   const handleThemeChange = () => {
     setDarkMode(!darkMode);
   };
+
+  /**
+  Criar componentes
+    Header
+      - Logo
+      - Nav (Desktop e Mobile)
+      - Botão de trocar tema
+    Main
+      Home
+        - Fundo animado
+        - Titulo (niveis h1, h2, h3)
+        - Avatar animado
+        - Barra com texto animado
+        - Botões de redes sociais
+      About
+        - Paragrafo
+        - Botão download curriculo
+      Projects
+        - Card de projetos
+          + Imagem Mockup
+          + Botões (ver projeto, ver codigo e ver design)
+          + Tecnologias (Colocar o nome ao passar o mouse ou clicar)
+      Contact
+        - Formulário
+          + Nome
+          + Email
+          + Mensagem
+          + Botão enviar (usar nodeMailer)
+        - Botão voltar ao topo
+      Footer (botão redes sociais)      
+  */
   return (
     <div className={`${darkMode && 'dark'}`}>
       <main className="dark:bg-blue-400 bg-white">
         <h1 className="dark:text-black bg-pink-400 dark:bg-yellow-300">
           Testando Tailwind
         </h1>
-        <button onClick={handleThemeChange}>trocar tema</button>
+        <ThemeButton
+          darkMode={darkMode}
+          handleThemeChange={handleThemeChange}
+        />
       </main>
     </div>
   );
