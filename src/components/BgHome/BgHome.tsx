b12efbe34d59413c35c026b526b86b1193fcb5eb
Bruno H <riquebruno@yahoo.com.br>
2024-10-09 20:56:03 -0300

    feat: cria componente BgHome + css

---
+++ src/components/BgHome/BgHome.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../../style/Bg.css';
+
+export default function BgHome() {
+  function getRandomInRange(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+  }
+
+  function getRandomNumber(): number {
+    const options = [1, 2];
+    return options[Math.floor(Math.random() * options.length)];
+  }
+
+  const divStyles = Array.from({ length: 16 }, () => ({
+    color: ['#581937', '#e45a84', '#fa00c4'][Math.floor(Math.random() * 3)],
+    top: `${getRandomInRange(10, 60)}%`,
+    left: `${getRandomInRange(0, 98)}%`,
+    animationDuration: `${getRandomInRange(20, 50)}s`,
+    animationDelay: `-${getRandomInRange(10, 50)}s`,
+    transformOrigin: `${getRandomInRange(-30, 30)}vw ${getRandomInRange(
+      -30,
+      30
+    )}vh`,
+    boxShadow: `${getRandomInRange(-40, 20)}vmin 0 ${getRandomInRange(
+      5,
+      6
+    )}vmin currentColor`,
+    size: `${getRandomNumber()}rem`,
+    rounded: `${getRandomInRange(14, 50)}%`,
+  }));
+
+  return (
+    <div className="h-full w-full absolute left-0 top-0 background z-0 md:block hidden">
+      {divStyles.map((style, index) => (
+        <div
+          key={index}
+          className="border-2 border-primary opacity-50"
+          style={{
+            color: style.color,
+            top: style.top,
+            left: style.left,
+            animationDuration: style.animationDuration,
+            animationDelay: style.animationDelay,
+            transformOrigin: style.transformOrigin,
+            boxShadow: style.boxShadow,
+            position: 'absolute',
+            height: style.size,
+            width: style.size,
+            borderRadius: style.rounded,
+          }}
+        />
+      ))}
+    </div>
+  );
+}
