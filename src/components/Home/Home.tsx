6b74556e3c9f062e7b224e52a65142706c8eb95a
Bruno H <riquebruno@yahoo.com.br>
2024-10-09 13:41:09 -0300

    feat: cria componente Home

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <section className="dark:text-text-dark h-screen pt-24">
+      <h1>Olá, eu sou o Bruno</h1>
+      <h2>Desenvolvedor Front-end</h2>
+      <h3>
+        Construo sites e aplicações web com as melhores tecnologias do mercado
+      </h3>
+      <button>Conheça meu trabalho</button>
+      <button>Entre em contato</button>
+    </section>
+  );
+}
