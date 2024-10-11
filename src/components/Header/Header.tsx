a7ebd6dbbfd41abe72e239afdae2d710194c7658
Bruno H <riquebruno@yahoo.com.br>
2024-10-11 18:31:49 -0300

    refactor: adiciona padding top de 20 em Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -50,7 +50,7 @@ export default function Header({ darkMode, handleThemeChange }: HeaderProps) {
     <header
       className={`${
         open ? 'h-96' : 'h-20'
-      } h-20 md:flex md:justify-around md:items-center fixed md:relative w-full`}
+      } h-20 md:flex md:justify-around md:items-center pt-20 fixed md:relative w-full`}
     >
       <div
         className={`${
