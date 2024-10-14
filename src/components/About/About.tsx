518e2eac283b5cda6f221b0574e7e702ca884a76
Bruno H <riquebruno@yahoo.com.br>
2024-10-14 10:43:42 -0300

    feat: cria componente About

---
+++ src/components/About/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+      </div>
+    </section>
+  );
+}
