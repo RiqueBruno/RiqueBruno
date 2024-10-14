65fd274e9644f17bd92b9952793d174fbba60545
Bruno H <riquebruno@yahoo.com.br>
2024-10-14 13:56:35 -0300

    refactor: melhora localização do link para uma melhor experincia

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -13,9 +13,10 @@ type HomeProps = {
 export default function Home({ darkMode }: HomeProps) {
   return (
     <section className="dark:text-text-dark h-[780px] lg:h-[calc(100vh-5rem)] md:h-[calc(100vh-8rem)] pt-[38rem] md:pt-20 lg:pt-24 w-full relative overflow-hidden lg:overflow-auto">
+      <div id="home" className="absolute top-0" />
       <div className="lg:grid lg:grid-cols-2 w-full h-full z-40 pb-20 flex flex-col items-center justify-center">
         <div className="flex flex-col justify-center items-center lg:items-start space-y-4 lg:pl-60 dark:text-text-dark text-text-light">
-          <LinkMenu text="Saudações!" emote="👋" id="home" />
+          <LinkMenu text="Saudações!" emote="👋" id="" />
           <div className="lg:hidden rounded-full overflow-hidden h-52 w-52 border-2 border-primary z-50 shadow-md">
             <img
               src={brunoImage}
