71d141f0d59ec178d9541d870f2a027a1a576004
Bruno H <riquebruno@yahoo.com.br>
2024-10-14 01:51:38 -0300

    refactor: Atualiza SocialMedia incluindo icons de forma dinamica

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import TechIcon from '../Skill/Skill';
 
-export default function SocialMedia() {
-  return <div>SocialMedia</div>;
+type SocialMediaProps = {
+  type: 'Whatsapp' | 'Linkedin' | 'Github';
+  size: number;
+  location: 'home' | 'footer';
+  linkTo: string;
+};
+
+export default function SocialMedia({
+  type,
+  size,
+  location,
+  linkTo,
+}: SocialMediaProps) {
+  return (
+    <a
+      href={linkTo}
+      className={`${
+        location === 'home' ? 'bg-primary' : 'bg-secondary'
+      } rounded-full w-12 h-12 flex justify-center items-center`}
+    >
+      <TechIcon name={type} size={size} color="#500742" />
+    </a>
+  );
 }
