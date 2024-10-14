5a823579bea4e20aa0c52928cfbf44e964bdbc84
Bruno H <riquebruno@yahoo.com.br>
2024-10-14 11:13:04 -0300

    feat: adiciona foto ao About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import brunoImage from '../../assets/images/foto.jpg';
 
 export default function About() {
   return (
-    <section>
+    <section className="bg-primary flex flex-col justify-center items-center lg:flex-row p-10">
       <div>
-        <div>
-          <img src="" alt="" />
+        <div className="lg:block rounded-full overflow-hidden h-52 w-52 border-2 border-primary z-50 shadow-md">
+          <img
+            src={brunoImage}
+            alt="Imagem de Bruno"
+            className="lg:block w-full h-full object-cover"
+          />
         </div>
         <div>
           <h2></h2>
