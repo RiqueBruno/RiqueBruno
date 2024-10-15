6db77438414f19becc87f7a30187d0d02f2d3706
Bruno H <riquebruno@yahoo.com.br>
2024-10-15 19:16:07 -0300

    refactor: remove imageOpen prop de imagePreview

--- src/components/ImagePreview/ImagePreview.tsx
+++ src/components/ImagePreview/ImagePreview.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 
 type ImagePreviewProps = {
-  imageOpen: boolean;
   setImageOpen: (imageOpen: boolean) => void;
   image: string;
 };
 
 export default function ImagePreview({
-  imageOpen,
   setImageOpen,
   image,
 }: ImagePreviewProps) {
   return (
     <div>
-      {imageOpen && (
-        <div>
-          <button onClick={() => setImageOpen(false)}>X</button>
-          <img src={image} alt="Imagem do projeto" />
-        </div>
-      )}
+      <div>
+        <button onClick={() => setImageOpen(false)}>X</button>
+        <img src={image} alt="Imagem do projeto" />
+      </div>
     </div>
   );
 }
