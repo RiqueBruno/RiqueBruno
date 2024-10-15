79d3798ad47154fef5df373b8f4dc46e1c020df7
Bruno H <riquebruno@yahoo.com.br>
2024-10-15 14:50:06 -0300

    feat: cria util para organizar as informações dos projetos

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -12,13 +12,15 @@ type Project = {
 const projects: Project[] = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    skills: ['React', 'TypeScript', 'Styled Components'],
-    deployLink: 'https://example.com',
-    codeLink: 'https://github.com',
-    designLink: 'https://dribbble.com',
-    image: 'project1.png',
+    title: 'Basic Shop',
+    description:
+      'Um e-commerce inspirado no site do Mercado Livre, desenvolvido com React, Tailwind CSS e TypeScript. Utilizando a API do Mercado Livre, o projeto permite a busca de produtos, categorização, adição de itens aos favoritos e ao carrinho. Com uma interface moderna e responsiva, oferece uma experiência de compra eficiente e intuitiva.',
+    skills: ['React', 'TypeScript', 'Tailwind'],
+    deployLink: 'https://basic-shop-nine.vercel.app/',
+    codeLink: 'https://github.com/RiqueBruno/basicShop',
+    designLink:
+      'https://www.figma.com/design/0QmmjgyPvPlP8dJS48Zr0X/BasicShop?node-id=0-1',
+    image: '../assets/mockup/basicShopMockup.png',
   },
 ];
 
