b360d286c18aec29f8228c0dc93cd7e77135e64d
Bruno H <riquebruno@yahoo.com.br>
2024-10-15 15:39:46 -0300

    refactor: adiciona funcionalidade ao card, recebendo props.

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -1,5 +1,53 @@
 import React from 'react';
+import TechIcon from '../Skill/Skill';
+import { TechName } from '../Skill/Skill';
 
-export default function CardProject() {
-  return <div>CardProject</div>;
+type ProjectProps = {
+  title: string;
+  description: string;
+  skills: TechName[];
+  deployLink: string;
+  codeLink: string;
+  designLink: string;
+  image: string;
+  imagePreview: string;
+};
+
+export default function CardProject({
+  title,
+  description,
+  skills,
+  deployLink,
+  codeLink,
+  designLink,
+  image,
+}: ProjectProps) {
+  return (
+    <article>
+      <div>
+        <header>
+          <h3>{title}</h3>
+        </header>
+        <div>
+          <p>{description}</p>
+        </div>
+        <div>
+          <a href={deployLink}>Deploy</a>
+          <a href={codeLink}>Code</a>
+          <a href={designLink}>Design</a>
+        </div>
+        <div>
+          {skills.map((skill, index) => (
+            <div key={index}>
+              <TechIcon name={skill} />
+              <p>{skill}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <img src={image} alt={title} />
+      </div>
+    </article>
+  );
 }
