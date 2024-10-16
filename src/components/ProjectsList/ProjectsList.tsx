42d988095282f660dcddf19060bc502ad1007062
Bruno H <riquebruno@yahoo.com.br>
2024-10-16 16:54:18 -0300

    refactor: refatora codigo de projectsList adicionando header e espaço para renderizar os projetos

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import projects from '../../utils/Projects';
+import CardProject from '../CardProject/CardProject';
+import LinkMenu from '../LinkMenu/LinkMenu';
 
 type ProjectProps = {
   imageOpen: boolean;
@@ -9,5 +12,30 @@ export default function ProjectsList({
   imageOpen,
   setImageOpen,
 }: ProjectProps) {
-  return <div>ProjectsList</div>;
+  return (
+    <section className="h-screen w-screen relative flex flex-col items-start justify-center p-4">
+      <div id="projects" className="absolute top-0 left-0" />
+      <header className="w-full flex flex-col items-center justify-center dark:text-text-dark">
+        <LinkMenu text="Projetos" id="" emote="🔗" />
+        <h2 className="text-3xl my-8 w-full text-center">Meus Projetos</h2>
+      </header>
+      <div className="h-[58%]">
+        {projects.map((project) => (
+          <CardProject
+            key={project.id}
+            imageOpen={imageOpen}
+            setImageOpen={setImageOpen}
+            title={project.title}
+            description={project.description}
+            skills={project.skills}
+            deployLink={project.deployLink}
+            codeLink={project.codeLink}
+            designLink={project.designLink}
+            image={project.image}
+            imagePreview={project.imagePreview}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
