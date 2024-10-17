5e70e7f2ebb8936fb0530aa9b38e729d11335714
Bruno H <riquebruno@yahoo.com.br>
2024-10-17 12:08:21 -0300

    refactor: adiciona funcionalidade ao LinkCard. Agora recebe props e renderiza um a

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { GrDeploy } from 'react-icons/gr';
+import { IoLogoFigma } from 'react-icons/io5';
+import { FaCode } from 'react-icons/fa';
 
-export default function LinkCard() {
-  return <div>LinkCard</div>;
+type LinkCardProps = {
+  type: 'Deploy' | 'Code' | 'Design';
+  link: string;
+};
+
+export default function LinkCard({ type, link }: LinkCardProps) {
+  const icons = {
+    Deploy: GrDeploy,
+    Design: IoLogoFigma,
+    Code: FaCode,
+  };
+  const Icon = icons[type];
+  return (
+    <a href={link}>
+      <Icon />
+      <span>{type}</span>
+    </a>
+  );
 }
