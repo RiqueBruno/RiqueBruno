22caa0f6fe5f52561c90f55effee985e4aece8d3
Bruno H <riquebruno@yahoo.com.br>
2024-10-17 15:11:04 -0300

    style: estiliza LinkCard

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -16,9 +16,14 @@ export default function LinkCard({ type, link }: LinkCardProps) {
   };
   const Icon = icons[type];
   return (
-    <a href={link}>
-      <Icon />
-      <span>{type}</span>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="dark:bg-btnCard-dark dark:hover:bg-btnCard-lightHover bg-btnCard-light hover:bg-btnCard-lightHover rounded-md p-2 dark:text-primary flex justify-center items-center space-x-1 mb-2"
+    >
+      <Icon size={15} />
+      <span className="text-sm dark:opacity-80">{type}</span>
     </a>
   );
 }
