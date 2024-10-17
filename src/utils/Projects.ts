62bebc6d54ff1c8eba4c1e2df3f68e1bf24c2be6
Bruno H <riquebruno@yahoo.com.br>
2024-10-17 10:30:18 -0300

    refactor: atualiza a forma de endereço da imagem de link direto para import

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -1,4 +1,5 @@
 import { TechName } from '../components/Skill/Skill';
+import basicShopMockup from '../assets/mockup/basicShopMockUp.png';
 
 type Project = {
   id: number;
@@ -23,7 +24,7 @@ const projects: Project[] = [
     codeLink: 'https://github.com/RiqueBruno/basicShop',
     designLink:
       'https://www.figma.com/design/0QmmjgyPvPlP8dJS48Zr0X/BasicShop?node-id=0-1',
-    image: '../assets/mockup/basicShopMockup.png',
+    image: basicShopMockup,
     imagePreview: '../assets/mockup/basicShopMockupPreview.png',
   },
 ];
