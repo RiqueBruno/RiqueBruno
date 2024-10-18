6a1f59d47ff7392865c21a15f1254ba83c508b96
Bruno H <riquebruno@yahoo.com.br>
2024-10-18 17:25:12 -0300

    refactor: adiciona outros 3 projetos ao utils Projects

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -1,8 +1,11 @@
 import { TechName } from '../components/Skill/Skill';
 import basicShopMockup from '../assets/mockup/basicShopMockUp.png';
 import recipesAppMockup from '../assets/mockup/recipeAppMockUp.png';
+import ttunesMockup from '../assets/mockup/TTunoAppMockUp.png';
+import wexpensesMockup from '../assets/mockup/WEAppMockUp.png';
+import triviaChallengerMockup from '../assets/mockup/triviaAppMockUp.png';
 
-type Project = {
+export type Project = {
   id: number;
   title: string;
   description: string;
@@ -41,6 +44,44 @@ const projects: Project[] = [
       'https://www.figma.com/design/bYMrChGsGyTjMadfTiYcwU/cook-yourself?node-id=0-1&node-type=canvas&t=d6jDxZBIVagBfkBa-0',
     image: recipesAppMockup,
     imagePreview: recipesAppMockup,
+  },
+  {
+    id: 3,
+    title: 'TTunes',
+    description:
+      'Aplicativo de músicas, onde o usuário pode fazer login, pesquisar, ouvir prévias de músicas e favoritar as que mais gostar.',
+    skills: ['React', 'CSS', 'JavaScript', 'Figma'],
+    deployLink: 'https://project-t-tunes.vercel.app/',
+    codeLink: 'https://github.com/RiqueBruno/projectTTunes',
+    designLink:
+      'https://www.figma.com/design/PNE3HkWzdo3t0f6fEpFah3/TTunes?t=lo4qU3a9iNeUN2j0-0',
+    image: ttunesMockup,
+    imagePreview: ttunesMockup,
+  },
+  {
+    id: 4,
+    title: 'WExpenses',
+    description:
+      'Aplicativo de controle de despesas, onde o usuário pode adicionar, editar e excluir despesas, além de visualizar as despesas totais. O projeto foi desenvolvido durante o curso da Trybe.',
+    skills: ['React', 'CSS', 'Redux', 'Figma'],
+    deployLink: 'https://wexpenses.vercel.app/',
+    codeLink: 'https://github.com/RiqueBruno/WExpenses',
+    designLink:
+      'https://www.figma.com/design/ZhABetebtQ17jdpxrl3HG1/WExpenses?t=lo4qU3a9iNeUN2j0-0',
+    image: wexpensesMockup,
+    imagePreview: wexpensesMockup,
+  },
+  {
+    id: 5,
+    title: 'Trivia Challenger',
+    description:
+      'Jogo de perguntas e respostas, onde o usuário pode responder perguntas de múltipla escolha. O projeto foi desenvolvido durante o curso da Trybe.',
+    skills: ['React', 'CSS', 'Redux', 'Figma'],
+    deployLink: 'https://trivia-challenger.vercel.app/',
+    codeLink: 'https://github.com/RiqueBruno/trivia-challenger',
+    designLink: '',
+    image: triviaChallengerMockup,
+    imagePreview: triviaChallengerMockup,
   },
 ];
 
