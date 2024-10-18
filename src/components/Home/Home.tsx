e400eebd24cc33297e928294039835b1669a5271
Bruno H <riquebruno@yahoo.com.br>
2024-10-18 17:22:21 -0300

    refactor: adiciona uma nova div ao Home para posicionamento melhor do texto(H1 H2) na tela

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -15,7 +15,7 @@ export default function Home({ darkMode }: HomeProps) {
     <section className="dark:text-text-dark h-[780px] lg:h-[calc(100vh)] md:h-[calc(100vh-8rem)] pt-[38rem] md:pt-20 lg:pt-24 w-full relative overflow-hidden lg:overflow-auto">
       <div id="home" className="absolute top-0" />
       <div className="lg:grid lg:grid-cols-2 w-full h-full z-40 pb-20 flex flex-col items-center justify-center">
-        <div className="flex flex-col justify-center items-center lg:items-start space-y-4 lg:pl-60 dark:text-text-dark text-text-light">
+        <div className="flex flex-col justify-center items-center space-y-4 dark:text-text-dark text-text-light">
           <LinkMenu text="Saudações!" emote="👋" id="" />
           <div className="lg:hidden rounded-full overflow-hidden h-52 w-52 border-2 border-primary z-50 shadow-md">
             <img
@@ -24,36 +24,38 @@ export default function Home({ darkMode }: HomeProps) {
               className="lg:hidden w-full h-full object-cover"
             />
           </div>
-          <h1 className="lg:text-5xl text-3xl font-bold z-[100]">
-            Bruno&nbsp;
-            <br className="hidden lg:block" />
-            Henrique
-          </h1>
-          <h2 className="lg:text-2xl text-lg z-[100]">
-            Front-end developer · UI designer
-          </h2>
-          <div className="lg:space-x-4 flex justify-around lg:justify-start lg:items-start lg:px-0 w-full px-10 z-[100]">
-            <SocialMedia
-              darkMode={darkMode}
-              type="Linkedin"
-              size={30}
-              location="home"
-              linkTo="https://www.linkedin.com/in/brunohenriquec"
-            />
-            <SocialMedia
-              darkMode={darkMode}
-              type="Github"
-              size={30}
-              location="home"
-              linkTo="https://github.com/RiqueBruno"
-            />
-            <SocialMedia
-              darkMode={darkMode}
-              type="Whatsapp"
-              size={30}
-              location="home"
-              linkTo="https://wa.me/5521993457722"
-            />
+          <div>
+            <h1 className="lg:text-5xl text-3xl font-bold z-[100]">
+              Bruno&nbsp;
+              <br className="hidden lg:block" />
+              Henrique
+            </h1>
+            <h2 className="lg:text-2xl text-lg z-[100]">
+              Front-end developer · UI designer
+            </h2>
+            <div className="lg:space-x-4 flex justify-around lg:justify-start lg:items-start lg:px-0 w-full px-10 z-[100]">
+              <SocialMedia
+                darkMode={darkMode}
+                type="Linkedin"
+                size={30}
+                location="home"
+                linkTo="https://www.linkedin.com/in/brunohenriquec"
+              />
+              <SocialMedia
+                darkMode={darkMode}
+                type="Github"
+                size={30}
+                location="home"
+                linkTo="https://github.com/RiqueBruno"
+              />
+              <SocialMedia
+                darkMode={darkMode}
+                type="Whatsapp"
+                size={30}
+                location="home"
+                linkTo="https://wa.me/5521993457722"
+              />
+            </div>
           </div>
         </div>
 
