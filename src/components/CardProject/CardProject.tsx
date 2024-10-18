06f684380187e236a1f9f82c109f652b448edf0e
Bruno H <riquebruno@yahoo.com.br>
2024-10-18 17:20:44 -0300

    refactor: estilos css

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -3,7 +3,8 @@ import LinkCard from '../LinkCard/LinkCard';
 import TechIcon from '../Skill/Skill';
 import { TechName } from '../Skill/Skill';
 import { IoExpand } from 'react-icons/io5';
-import '../../style/Bg.css';
+import '../../styles/Bg.css';
+import '../../styles/scroll.css';
 
 type ProjectProps = {
   id: number;
@@ -42,9 +43,9 @@ export default function CardProject({
       : description;
 
   return (
-    <article className="h-full w-72 lg:h-[32rem] lg:w-[32rem] rounded-md border-2 border-primary-light relative dark:text-text-dark text-text-light">
+    <article className="h-full w-72 md:h-[90%] lg:h-[90%] xl:w-[32rem] xl:h-[32rem] lg:w-[32rem] flex-none rounded-md border-2 border-primary-light relative dark:text-text-dark text-text-light">
       <div className="w-full h-full dark:bg-card-dark flex flex-col justify-center items-center z-[50] p-2">
-        <div className="z-[51] h-[24%] w-full mb-4 relative">
+        <div className="z-[51] h-[24%] lg:h-16 w-full mb-4 relative">
           <img
             src={image}
             alt={title}
@@ -60,7 +61,7 @@ export default function CardProject({
             <IoExpand />
           </button>
         </div>
-        <header className="z-[51] text-2xl mb-2 lg:mb-4">
+        <header className="z-[51] text-2xl mb-2 lg:mb-2 xl:mb-4">
           <h3>{title}</h3>
         </header>
         <div className="z-[51] text-sm text-center mb-2 lg:mb-2 xl:mb-8 border-b-2 border-transparent w-[80%] opacity-70 pb-2">
@@ -77,18 +78,18 @@ export default function CardProject({
             )}
           </p>
         </div>
-        <div className="z-[51] border-b-2 border-transparent w-[90%] lg:w-[80%] flex justify-around mb-4">
+        <div className="z-[51] border-b-2 border-transparent w-[90%] lg:w-[80%] flex justify-around lg:mb-2 mb-4">
           <LinkCard link={deployLink} type="Deploy" />
           <LinkCard link={codeLink} type="Code" />
           {designLink.length > 5 && (
             <LinkCard link={designLink} type="Design" />
           )}
         </div>
-        <div className="z-[51] flex flex-wrap w-[90%] opacity-50 justify-center lg:mt-4">
+        <div className="z-[51] flex flex-wrap w-[90%] opacity-50 justify-center lg:mt-2 xl:mt-4">
           {skills.map((skill, index) => (
             <div
               key={index}
-              className="flex flex-col text-xs lg:text-base justify-center items-center text-center space-y-1 mb-2 mr-4 dark:text-text-dark"
+              className="flex flex-col text-xs xl:text-base justify-center items-center text-center space-y-1 mb-2 mr-4 dark:text-text-dark"
             >
               <TechIcon name={skill} color="" />
               <p>{skill}</p>
