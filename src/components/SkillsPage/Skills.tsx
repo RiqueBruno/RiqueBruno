a62b13ad20e8406c93dc5eed3bb2b1df72381d91
Bruno H <riquebruno@yahoo.com.br>
2024-10-21 19:21:39 -0300

    refactor: estilo para componente Skills

--- src/components/SkillsPage/Skills.tsx
+++ src/components/SkillsPage/Skills.tsx
@@ -26,27 +26,42 @@ export default function Skills() {
   ];
 
   return (
-    <section className="h-screen w-screen relative dark:text-text-dark text-text-light">
+    <section className="h-screen w-screen relative dark:text-text-dark text-text-light flex flex-col items-center justify-end pb-14 space-y-4 overflow-hidden">
       <div className="absolute h-full w-full inset-0 border-x-2 border-primary rounded-lg z-[0]" />
-      <LinkMenu text="Minhas Skills" emote="🧐" id="" />
-      <h2>Skills</h2>
-      <div>
-        <section>
-          <h3>Skill e Framework que mais uso:</h3>
-          <ul className="text-primary">
+      <div className="absolute h-full w-full inset-0 z-[0] text-[24rem] text-center opacity-10 hidden lg:block">
+        <div>MINHAS</div>
+        <div>TECNOLOGIAS</div>
+      </div>
+      <div className="z-[500] flex flex-col justify-center items-center space-y-4">
+        <LinkMenu text="Minhas Skills" emote="🧐" id="" />
+        <h2 className="text-3xl lg:pb-14" id="skills">
+          Tecnologias
+        </h2>
+      </div>
+      <div className="lg:h-[60%] z-[500] lg:w-[60%] w-full flex flex-col justify-center items-center rounded-lg lg:border-2 lg:border-primary-light space-y-8 lg:shadow-custom-magenta dark:bg-background-dark bg-background-light">
+        <section className="flex flex-col items-center justify-center w-[90%] space-y-8">
+          <h3 className="text-lg opacity-80">Tecnologias que mais uso:</h3>
+          <ul className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-6 w-full text-center">
             {arrSkills.slice(0, 9).map((skill) => (
-              <li key={skill}>
+              <li
+                key={skill}
+                className="dark:opacity-50 h-10 lg:h-14 rounded-md opacity-80 text-xs md:text-sm lg:text-base hover:opacity-100 dark:hover:opacity-90 hover:bg-transparent transition-opacity duration-300 flex items-center justify-center flex-col dark:text-text-dark text-text-light"
+              >
                 <Skill name={skill} />
                 <p>{skill}</p>
               </li>
             ))}
           </ul>
         </section>
-        <section>
-          <h3>Skill e Framework que já usei:</h3>
-          <ul>
+        <div className="w-[90%] border-b-2 border-primary opacity-50" />
+        <section className="flex flex-col items-center justify-center w-[90%] space-y-8">
+          <h3 className="text-lg opacity-80">Tecnologias que já usei:</h3>
+          <ul className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 w-full text-center">
             {arrSkills.slice(9).map((skill) => (
-              <li key={skill}>
+              <li
+                key={skill}
+                className="dark:opacity-50 h-10 lg:h-14 rounded-md opacity-80 text-xs md:text-sm lg:text-base hover:opacity-100 dark:hover:opacity-90 hover:bg-transparent transition-opacity duration-300 flex items-center justify-center flex-col dark:text-text-dark text-text-light"
+              >
                 <Skill name={skill} />
                 <p>{skill}</p>
               </li>
