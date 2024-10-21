5602ab0ad41b81d2785f5b235bf1e493b4b47255
Bruno H <riquebruno@yahoo.com.br>
2024-10-21 12:28:10 -0300

    refactor: adiciona skills ao componente Skills

--- src/components/SkillsPage/Skills.tsx
+++ src/components/SkillsPage/Skills.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import LinkMenu from '../LinkMenu/LinkMenu';
-import Skill from '../Skill/Skill';
+import Skill, { TechName } from '../Skill/Skill';
 
 export default function Skills() {
+  const arrSkills: TechName[] = [
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'CSS',
+    'Tailwind',
+    'HTML5',
+    'Git',
+    'Github',
+    'Figma',
+    'Photoshop',
+    'Illustrator',
+    'Node',
+    'Nextjs',
+    'Insomnia',
+    'Docker',
+    'Vite',
+    'Postman',
+    'Redux',
+    'SASS',
+  ];
+
   return (
     <section className="h-screen w-screen relative dark:text-text-dark text-text-light">
       <div className="absolute h-full w-full inset-0 border-x-2 border-primary rounded-lg z-[0]" />
@@ -12,87 +34,23 @@ export default function Skills() {
         <section>
           <h3>Skill e Framework que mais uso:</h3>
           <ul className="text-primary">
-            <li>
-              <Skill name="JavaScript" />
-              <p>JavaScript</p>
-            </li>
-            <li>
-              <Skill name="TypeScript" />
-              <p>TypeScript</p>
-            </li>
-            <li>
-              <Skill name="React" />
-              <p>React</p>
-            </li>
-            <li>
-              <Skill name="CSS" />
-              <p>CSS</p>
-            </li>
-            <li>
-              <Skill name="Tailwind" />
-              <p>Tailwind</p>
-            </li>
-            <li>
-              <Skill name="HTML5" />
-              <p>HTML5</p>
-            </li>
-            <li>
-              <Skill name="Git" />
-              <p>Git</p>
-            </li>
-            <li>
-              <Skill name="Github" />
-              <p>GitHub</p>
-            </li>
-            <li>
-              <Skill name="Figma" />
-              <p>Figma</p>
-            </li>
+            {arrSkills.slice(0, 9).map((skill) => (
+              <li key={skill}>
+                <Skill name={skill} />
+                <p>{skill}</p>
+              </li>
+            ))}
           </ul>
         </section>
         <section>
           <h3>Skill e Framework que já usei:</h3>
           <ul>
-            <li>
-              <Skill name="Photoshop" />
-              <p>Photoshop</p>
-            </li>
-            <li>
-              <Skill name="Illustrator" />
-              <p>Illustrator</p>
-            </li>
-            <li>
-              <Skill name="Node" />
-              <p>Node</p>
-            </li>
-            <li>
-              <Skill name="Nextjs" />
-              <p>Nextjs</p>
-            </li>
-            <li>
-              <Skill name="Insomnia" />
-              <p>Insomnia</p>
-            </li>
-            <li>
-              <Skill name="Docker" />
-              <p>Docker</p>
-            </li>
-            <li>
-              <Skill name="Vite" />
-              <p>Vite</p>
-            </li>
-            <li>
-              <Skill name="Postman" />
-              <p>Postman</p>
-            </li>
-            <li>
-              <Skill name="Redux" />
-              <p>Redux</p>
-            </li>
-            <li>
-              <Skill name="SASS" />
-              <p>SASS</p>
-            </li>
+            {arrSkills.slice(9).map((skill) => (
+              <li key={skill}>
+                <Skill name={skill} />
+                <p>{skill}</p>
+              </li>
+            ))}
           </ul>
         </section>
       </div>
