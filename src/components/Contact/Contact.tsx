fbf38dbe6b9faf89115bca32a07dbd9cca26aff1
Bruno H <riquebruno@yahoo.com.br>
2024-10-22 14:35:30 -0300

    refactor: Contact - adiciona header e form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <header>
+        <h2>Contato</h2>
+      </header>
+      <form>
+        <label htmlFor="name">Nome</label>
+        <input type="text" id="name" name="name" required />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required />
+        <label htmlFor="message">Mensagem</label>
+        <textarea id="message" name="message" required></textarea>
+        <button type="submit">Enviar</button>
+      </form>
+      <button>Voltar ao topo</button>
+    </section>
+  );
 }
