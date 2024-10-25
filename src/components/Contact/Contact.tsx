d39e9a14e64e98207dfd59d5f70a3ef416dff988
Bruno H <riquebruno@yahoo.com.br>
2024-10-25 14:23:44 -0300

    refactor: Contact - remove form adicinando componente Form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,6 +3,7 @@ import LinkMenu from '../LinkMenu/LinkMenu';
 import contactMe from '../../assets/images/Contact.png';
 import { FaArrowUp } from 'react-icons/fa6';
 import '../../styles/Label.css';
+import Form from '../Form/Form';
 
 export default function Contact() {
   return (
@@ -18,61 +19,8 @@ export default function Contact() {
         <div className="hidden w-[50%] lg:flex items-center justify-center">
           <img src={contactMe} alt="Entre em contato comigo." />
         </div>
-        <div className="lg:w-[50%] w-full flex justify-center lg:justify-start items-center">
-          <form className="flex flex-col p-4 space-y-8 items-start justify-center w-[84%]">
-            <div className="w-full relative h-12 div-label">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder=" "
-                required
-                className="dark:bg-background-dark bg-background-light dark:text-text-dark text-text-light border-2 border-primary rounded-lg p-2 w-full h-full"
-              />
-              <label
-                htmlFor="name"
-                className="label-label dark:bg-background-dark bg-background-light px-1"
-              >
-                Nome
-              </label>
-            </div>
-            <div className="w-full relative h-12 div-label">
-              <input
-                type="text"
-                id="email"
-                name="email"
-                placeholder=" "
-                required
-                className="dark:bg-background-dark bg-background-light dark:text-text-dark text-text-light border-2 border-primary rounded-lg w-full h-full"
-              />
-              <label
-                htmlFor="email"
-                className="label-label dark:bg-background-dark bg-background-light px-1"
-              >
-                Email
-              </label>
-            </div>
-            <div className="w-full relative max-h-44 div-label">
-              <textarea
-                id="message"
-                name="message"
-                required
-                className="dark:bg-background-dark bg-background-light dark:text-text-dark text-text-light border-2 border-primary rounded-lg p-2 w-full h-full max-h-44 resize-none"
-              ></textarea>
-              <label
-                htmlFor="message"
-                className="label-label dark:bg-background-dark bg-background-light px-1"
-              >
-                Mensagem
-              </label>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-gradient-to-r from-secondary-light to-primary hover:from-primary hover:to-secondary-light text-text-dark dark:hover:from-secondary-dark dark:hover:to-primary-dark p-2 rounded-lg"
-            >
-              Enviar
-            </button>
-          </form>
+        <div className="lg:w-[50%] w-full">
+          <Form />
         </div>
       </div>
       <div className="w-full h-28 flex justify-center items-end">
