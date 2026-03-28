1e6112aafea3582f4a5d1ae51cadbc613b6f11d5
Bruno <riquebruno@yahoo.com.br>
2026-03-28 00:57:11 -0300

    refactor: remove lógica antiga e adiciona uma nova usando o swiper para criar o carrossel dos projetos

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect, useState } from "react";
 import { Project } from "../../utils/Projects";
 import CardProject from "../CardProject/CardProject";
-import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
-import { GoDotFill } from "react-icons/go";
 import "../../styles/scroll.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 
 type CarouselProps = {
   arrProjects: Project[];
@@ -16,125 +18,44 @@ export default function Carousel({
   setImageOpen,
   setProjectId,
 }: CarouselProps) {
-  const carouselRef = useRef<HTMLDivElement>(null);
-  const cardRef = useRef<HTMLDivElement>(null);
-  const [cardWidth, setCardWidth] = useState(0);
-  const [currentIndexProject, setCurrentIndexProject] = useState(0);
-  const [isAtStart, setIsAtStart] = useState(true);
-  const [isAtEnd, setIsAtEnd] = useState(false);
-
-  useEffect(() => {
-    if (cardRef.current) {
-      setCardWidth(cardRef.current.offsetWidth);
-    }
-  }, []);
-
-  useEffect(() => {
-    const checkPosition = () => {
-      if (carouselRef.current) {
-        const { scrollLeft, scrollWidth, clientWidth } = carouselRef.current;
-        setIsAtStart(scrollLeft === 0);
-        setIsAtEnd(scrollLeft + clientWidth >= scrollWidth);
-      }
-    };
-
-    if (carouselRef.current) {
-      checkPosition();
-      carouselRef.current.addEventListener("scroll", checkPosition);
-    }
-
-    return () => {
-      if (carouselRef.current) {
-        carouselRef.current.removeEventListener("scroll", checkPosition);
-      }
-    };
-  }, [carouselRef]);
-
-  function handleScroll(
-    e: React.MouseEvent<HTMLButtonElement>,
-    direction: string,
-  ) {
-    e.preventDefault();
-
-    const spacing = 16;
-
-    if (carouselRef.current && cardWidth > 0) {
-      const { scrollLeft } = carouselRef.current;
-
-      if (direction === "left") {
-        carouselRef.current.scrollLeft = scrollLeft - (cardWidth + spacing);
-        if (currentIndexProject > 0) {
-          setCurrentIndexProject(currentIndexProject - 1);
-        }
-      } else if (direction === "right") {
-        carouselRef.current.scrollLeft = scrollLeft + (cardWidth + spacing);
-        if (currentIndexProject < arrProjects.length - 1) {
-          setCurrentIndexProject(currentIndexProject + 1);
-        }
-      }
-    }
-  }
-
   return (
-    <div className="h-full relative w-full border-r-2">
-      <div
-        ref={carouselRef}
-        className="scrollbar scroll-smooth h-full w-full border-2 overflow-hidden overflow-x-auto lg:select-none border-primary flex lg:items-center lg:p-4 lg:rounded-lg space-x-4"
-      >
-        {arrProjects.map((project, index) => (
-          <div
-            key={project.id}
-            ref={index === 0 ? cardRef : null}
-            className="w-72 md:h-[90%] lg:h-[90%] xl:w-[32rem] xl:h-[32rem] lg:w-[32rem] h-full"
-          >
-            <CardProject
-              key={project.id}
-              id={project.id}
-              setImageOpen={setImageOpen}
-              title={project.title}
-              description={project.description}
-              skills={project.skills}
-              deployLink={project.deployLink}
-              codeLink={project.codeLink}
-              designLink={project.designLink}
-              image={project.image}
-              setProjectId={setProjectId}
-            />
-          </div>
-        ))}
-      </div>
-      <button
-        id="left"
-        aria-label="Ver projeto anterior"
-        disabled={isAtStart}
-        onClick={(e) => handleScroll(e, "left")}
-        className="absolute p-[0.2rem] z-40 left-0 top-[50%] translate-x-[-100%] translate-y-[-50%] text-4xl bg-primary-light rounded-l-full dark:text-black text-text-light hover:text-text-dark dark:hover:text-secondary-light disabled:bg-opacity-60"
-      >
-        <FaChevronCircleLeft />
-      </button>
-      <button
-        id="right"
-        aria-label="Ver próximo projeto"
-        disabled={isAtEnd}
-        onClick={(e) => handleScroll(e, "right")}
-        className="absolute p-[0.2rem] z-40 right-0 top-[50%] translate-x-[100%] translate-y-[-50%] text-4xl bg-primary-light rounded-r-full dark:text-black text-text-light hover:text-text-dark dark:hover:text-secondary-light disabled:opacity-60"
-      >
-        <FaChevronCircleRight />
-      </button>
-      <div className="flex w-full items-center justify-center md:hidden">
-        {arrProjects.map((project) => (
-          <span
-            key={project.id}
-            className={`${
-              currentIndexProject === project.id - 1
-                ? "text-primary"
-                : "text-text-light"
-            } text-2xl`}
-          >
-            <GoDotFill />
-          </span>
-        ))}
-      </div>
-    </div>
+    <Swiper
+      spaceBetween={30}
+      loop={true}
+      pagination={{ clickable: true }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="w-full max-w-6xl py-8 h-96"
+      breakpoints={{
+        320: { slidesPerView: 1 },
+        1024: { slidesPerView: 2 },
+      }}
+      style={
+        {
+          "--swiper-pagination-color": "#E11D48",
+          "--swiper-navigation-color": "#E11D48",
+        } as React.CSSProperties
+      }
+    >
+      {arrProjects.map((project) => (
+        <SwiperSlide
+          key={project.id}
+          className="flex justify-center h-auto pb-12"
+        >
+          <CardProject
+            id={project.id}
+            setImageOpen={setImageOpen}
+            title={project.title}
+            description={project.description}
+            skills={project.skills}
+            deployLink={project.deployLink}
+            codeLink={project.codeLink}
+            designLink={project.designLink}
+            image={project.image}
+            setProjectId={setProjectId}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
