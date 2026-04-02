deae2e968eadaa51e7dbe8b033886068c75b807a
Bruno <riquebruno@yahoo.com.br>
2026-04-02 15:34:30 -0300

    feat: add Java skill in AvatarHome

--- src/components/AvatarHome/AvatarHome.tsx
+++ src/components/AvatarHome/AvatarHome.tsx
@@ -1,6 +1,6 @@
-import avatar from '../../assets/images/Avatar.webp';
-import TechIcon from '../Skill/Skill';
-import '../../styles/Bg.css';
+import avatar from "../../assets/images/Avatar.webp";
+import TechIcon from "../Skill/Skill";
+import "../../styles/Bg.css";
 
 type AvatarHomeProps = {
   darkMode: boolean;
@@ -15,28 +15,28 @@ export default function AvatarHome({ darkMode }: AvatarHomeProps) {
         <TechIcon
           name="React"
           size={30}
-          color={darkMode ? '#E24ACE' : '#FFFFFF'}
+          color={darkMode ? "#E24ACE" : "#FFFFFF"}
         />
       </div>
       <div className="absolute z-30 dark:bg-transparent bg-secondary-light h-16 w-16 flex items-center justify-center rounded-full right-0 bottom-10">
         <TechIcon
-          name="Figma"
+          name="Java"
           size={30}
-          color={darkMode ? '#E24ACE' : '#FFFFFF'}
+          color={darkMode ? "#E24ACE" : "#FFFFFF"}
         />
       </div>
       <div className="absolute z-30 dark:bg-transparent bg-secondary-light h-16 w-16 flex items-center justify-center rounded-md top-6 right-6">
         <TechIcon
           name="TypeScript"
           size={30}
-          color={darkMode ? '#E24ACE' : '#FFFFFF'}
+          color={darkMode ? "#E24ACE" : "#FFFFFF"}
         />
       </div>
       <div className="absolute z-30 dark:bg-transparent bg-secondary-light h-16 w-16 flex items-center justify-center rounded-full left-20 bottom-16">
         <TechIcon
           name="Tailwind"
           size={30}
-          color={darkMode ? '#E24ACE' : '#FFFFFF'}
+          color={darkMode ? "#E24ACE" : "#FFFFFF"}
         />
       </div>
       <div className="w-full h-full p-6 max-w-xs max-h-xs mx-auto z-20">
